5222278fd9811551ff4ba3b98f82a8a16b9af958
eljose003 <jose.flores.s@ug.uchile.cl>
2026-04-22 23:18:44 -0400

    completed some changes

--- autoloads/bullet.gd
+++ autoloads/bullet.gd
@@ -5,7 +5,10 @@ var speed = -750
 func _physics_process(delta):
 	position += transform.y * speed * delta
 
-func _on_Bullet_body_entered(body):
-	#if body.is_in_group("mobs"):
-	#    body.queue_free()
+func _on_Area2D_body_entered(body):
 	queue_free()
+func _on_body_entered(body):
+	print("hit!")
+	queue_free()
+func _ready() -> void:
+	area_entered.connect(_on_Area2D_body_entered)
